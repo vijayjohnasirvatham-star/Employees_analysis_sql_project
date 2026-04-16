4ef6838a84440a38637fa7911e955213de23104f
vijayjohnasirvatham-star <vijayjohnasirvatham@gmail.com>
2026-04-16 15:40:43 +0530

    Employee Data Analysis Project with SQL queries and Power BI dashboard

--- schema.sql
+++ schema.sql
@@ -1,12 +1,11 @@
 CREATE TABLE employees (
-id INT PRIMARY KEY,
-name VARCHAR(50),
-salary INT,
-dept_id INT,
-manager_id INT
-);
-
-CREATE TABLE departments (
-id INT PRIMARY KEY,
-dept_name VARCHAR(50)
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    gender VARCHAR(10),
+    age INT,
+    department VARCHAR(50),
+    salary DECIMAL(10,2),
+    experience_years INT,
+    joining_date DATE,
+    attrition VARCHAR(10)
 );
