3aaac5b3a929d0725147cd1d497531229a3321be
vijayjohnasirvatham-star <vijayjohnasirvatham@gmail.com>
2026-04-16 15:39:47 +0530

    Employee Data Analysis Project with SQL queries and Power BI dashboard

--- insert_data.sql
+++ insert_data.sql
@@ -1,11 +1,11 @@
-INSERT INTO departments VALUES
-(1,'HR'),
-(2,'IT'),
-(3,'Finance');
-
 INSERT INTO employees VALUES
-(1,'Alice',50000,1,3),
-(2,'Bob',60000,1,3),
-(3,'Charlie',70000,2,NULL),
-(4,'David',65000,2,3),
-(5,'Eva',55000,NULL,2);
+(1,'Amit','Male',28,'IT',60000,3,'2021-06-10','No'),
+(2,'Priya','Female',32,'HR',50000,6,'2019-03-15','Yes'),
+(3,'Rahul','Male',26,'Sales',45000,2,'2022-01-20','No'),
+(4,'Sneha','Female',30,'IT',70000,5,'2020-07-11','Yes'),
+(5,'Arjun','Male',35,'Finance',80000,8,'2018-05-05','No'),
+(6,'Kavya','Female',27,'Sales',48000,3,'2021-09-09','No'),
+(7,'Vikram','Male',40,'Finance',90000,12,'2015-11-23','Yes'),
+(8,'Neha','Female',29,'HR',52000,4,'2020-12-01','No'),
+(9,'Rohit','Male',31,'IT',65000,6,'2019-08-18','No'),
+(10,'Anjali','Female',33,'Sales',55000,7,'2018-10-30','Yes');
